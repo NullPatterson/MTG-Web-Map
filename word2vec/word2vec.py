7495813f8b40ea8c4b62c2212aa88861cc43dd06
Nol Patterson <nol.patterson1683@gmail.com>
2024-01-03 15:13:22 -0600

    Added word2vec models for the oracle text of cards

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -9,5 +9,29 @@ from gensim.models import Word2Vec
 import gensim 
 from nltk.tokenize import sent_tokenize, word_tokenize
 import warnings
+import json
 
 warnings.filterwarnings(action='ignore')
+
+# Start of added bits for testing, but will be moved to main.py after testing. maybe(?)
+f = open('oracle-cards-20231216220200.json')
+oracle_cards = json.load(f)
+
+# Iterate through each card's oracle_text
+embedding_data = []
+for text in oracle_cards['oracle_text']:
+    text = text.replace("\n", " ") # Replace escape caharacter with space
+    #Iterate through each sentence
+    for i in sent_tokenize(text):
+        temp = []
+
+        # tokenize the sentence into words
+        for j in word_tokenize(i):
+            temp.append(j.lower())
+        embedding_data.append(temp)
+
+# Creating a CBOW model
+cbow_model = gensim.models.Word2Vec(embedding_data, min_count=1, vector_size=100, window=7)
+
+# Creating a Skip Gram Model
+sg_model = gensim.models.Word2Vec(embedding_data, min_count=1, vector_size=100, window=7, sg=1)
